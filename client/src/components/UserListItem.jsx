6d50d35fed75a8f6f90ff143812efbeaae489b20
jovan <jovanpaloski@gmail.com>
2025-03-08 19:09:10 +0100

    Format iso date

--- client/src/components/UserListItem.jsx
+++ client/src/components/UserListItem.jsx
@@ -1,3 +1,5 @@
+import { fromIsoDate } from "../utils/datetimeUtils.js";
+
 export default function UserListItem({
     firstName, lastName, email, phoneNumber, createdAt, imageUrl
 }) {
@@ -11,7 +13,7 @@ export default function UserListItem({
             <td>{email}</td>
             <td>{phoneNumber}</td>
             {/* <td>June 28, 2022</td> */}
-            <td>{createdAt}</td>
+            <td>{fromIsoDate(createdAt)}</td>
 
             <td className="actions">
                 <button className="btn edit-btn" title="Edit">
